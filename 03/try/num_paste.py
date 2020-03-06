d536f973f464be6ceed00547b2fe370fb7a4ba05
lunarus00 <moongur1900@naver.com>
2020-03-06 22:45:25 +0900

    200306 | problem fail

---
+++ 03/try/num_paste.py
@@ -0,0 +1,37 @@
+import sys
+sys.stdin = open('num_paste.txt')
+
+T = int(input())
+
+for_i = [0, 1, 0, -1]
+for_j = [1, 0, -1, 0]
+
+def find_nums(i, j):
+    global check_nums, check_count, count_nums
+    if check_count == 7:
+        if check_nums not in check_list:
+            check_list.append(check_nums)
+            count_nums += 1
+            return
+    for k in range(4):
+        go_i = i + for_i[k]
+        go_j = j + for_j[k]
+        if 0 <= go_i < 4 and 0 <= go_j < 4:
+            check_count += 1
+            check_nums.append(matrix[go_i][go_j])
+            find_nums(go_i, go_j)
+            check_count -= 1
+            check_nums.pop()
+
+for tc in range(T):
+    matrix = []
+    for i in range(4):
+        matrix.append(list(map(str, input().split())))
+    count_nums = 0
+    check_list = []
+    for i in range(4):
+        for j in range(4):
+            check_count = 1
+            check_nums = [matrix[i][j]]
+            find_nums(i, j)
+    print(count_nums)
