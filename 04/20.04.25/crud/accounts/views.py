125e5f38b0687314ad47e3812a28c7d5e5de72c5
lunarus00 <moongur1900@naver.com>
2020-04-26 13:06:49 +0900

    200426 | 회원가입/로그인

--- 04/20.04.25/crud/accounts/views.py
+++ 04/20.04.25/crud/accounts/views.py
@@ -19,3 +19,20 @@ def signup(request):
 
 def index(request):
     return render(request, 'accounts/index.html')
+
+def login(request):
+    if request.mehtod == 'POST':
+        form = AthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index.html')
+    else:
+        form = AthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accoutns:index')
