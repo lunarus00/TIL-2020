87bcfe932ef9368756758521e16cdbd2b18cb5de
lunarus00 <moongur1900@naver.com>
2020-05-09 19:53:52 +0900

    200509 | forms.py에서 forms.Modelform 으로 form 형성/ update에서 POST 쪽 form에도 instance를 넣어줘야 새 파일이 만들어지지 않고 덮어씌워짐

--- 05/20.05.08~10/crud/articles/views.py
+++ 05/20.05.08~10/crud/articles/views.py
@@ -14,10 +14,36 @@ def create(request):
         form = ArticleForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('articles:index')
+            return redirect('articles:detail', article_pk)
     else:
         form = ArticleForm()
     context = {
             'form': form,
+    }
+    return render(request, 'articles/form.html', context)
+
+def detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(reqeust, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('articles:index')
+
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance = article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article_pk)
+    else:
+        form = ArticleForm(instance = article)
+    context = {
+        'form': form,
     }
     return render(request, 'articles/form.html', context)
