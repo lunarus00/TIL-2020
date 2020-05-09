67456b16bd936ecac9cee9f4c7684010b7106f63
lunarus00 <moongur1900@naver.com>
2020-05-09 12:41:25 +0900

    200509 | create

--- 05/20.05.08~10/crud/articles/views.py
+++ 05/20.05.08~10/crud/articles/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
 
 def index(request):
-    return render(request, 'articles/index.html')
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    context = {
+            'form': form,
+    }
+    return render(request, 'articles/form.html', context)
