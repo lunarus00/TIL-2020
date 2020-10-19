b90bd22cfae3498ae6b0b7e5e5a36ccc2d2dacb6
lunarus00 <moongur1900@naver.com>
2020-10-19 10:19:43 +0900

    201019 | 음식배달 실패

---
+++ 10/20.10.19/음식배달.py
@@ -0,0 +1,47 @@
+import sys, time
+
+sys.stdin = open('음식배달.txt')
+
+start_time = time.time()
+
+def find_food(number):
+    global min_val, check_val
+    y_num = number[0]
+    x_num = number[1]
+    check_val += map_list[y_num][x_num]
+    for k in range(1, N):
+        for l in range(1, N):
+            if map_list[k][l] == 1:
+                value = abs(k-y_num) + abs(l-x_num)
+                if visited[k][l] > value or visited[k][l] == 0:
+                    check_val -= visited[k][l]
+                    visited[k][l] = value
+                    check_val += value
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    map_list = []
+    for i in range(N):
+        map_list.append(list(map(int, input().split())))
+    food_list = []
+    for i in range(1, N):
+        for j in range(1, N):
+            if map_list[i][j] >= 2:
+                food_list.append((i, j))
+    food_length = len(food_list)
+    min_val = 262144
+    for i in range(food_length):
+        check_val = 0
+        visited = [[0] * N for j in range(N)]
+        for j in range(i, food_length):
+            find_food(food_list[j])
+            if check_val >= min_val:
+                break
+            elif j == food_length-1:
+                if check_val < min_val:
+                    min_val = check_val
+    print('#{} {}'.format(tc+1, min_val))
+
+print(start_time - time.time())
